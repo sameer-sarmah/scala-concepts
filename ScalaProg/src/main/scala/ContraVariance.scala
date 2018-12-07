41289f5ba56a82747e529fefbd3071740b87ef78
Sameer Sarmah <sameerparansarmah@gmail.com>
2018-12-07 14:58:06 +0530

    Update ContraVariance.scala

--- ScalaProg/src/main/scala/ContraVariance.scala
+++ ScalaProg/src/main/scala/ContraVariance.scala
@@ -4,15 +4,18 @@ case class FruitBasket[-T]() {
 }
 
 object ContraVariance extends App {
+  val objectBasket = FruitBasket[Any]()
   val appleBasket = FruitBasket[Apple]()
   val mcIntoshBasket = FruitBasket[McIntosh]()
   val orangeBasket = FruitBasket[Orange]()
   basketDetails(appleBasket)
   basketDetails(mcIntoshBasket)
+  basketDetails(objectBasket)
+
   //basketDetails(orangeBasket) not allowed
 
-  // accepts FruitBasket[McIntosh] or its superclass
-  //since Apple is a superclass of McIntosh ,Basket[Apple] is a superclass Basket[McIntosh]
+  // accepts FruitBasket[McIntosh] or its subclass
+  //since Apple is a superclass of McIntosh ,due to contravariance Basket[Apple] is a subclass Basket[McIntosh]
   def basketDetails(basket: FruitBasket[McIntosh]) = {
     println(s"this is a ${basket.name}")
   }
